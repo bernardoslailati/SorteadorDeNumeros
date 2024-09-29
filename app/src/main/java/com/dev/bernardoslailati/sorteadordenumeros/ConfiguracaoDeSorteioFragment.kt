0c6bfb11d96263f26a5f445adc43a432ffb13929
Bernardo Michel Slailati <bernardo-slailati@hotmail.com>
2024-09-29 15:38:34 +0200

    Initial commit

---
+++ app/src/main/java/com/dev/bernardoslailati/sorteadordenumeros/ConfiguracaoDeSorteioFragment.kt
@@ -0,0 +1,77 @@
+package com.dev.bernardoslailati.sorteadordenumeros
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources.getColorStateList
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.activityViewModels
+import com.dev.bernardoslailati.sorteadordenumeros.databinding.FragmentConfiguracaoDeSorteioBinding
+import com.dev.bernardoslailati.sorteadordenumeros.databinding.FragmentResultadoSorteioBinding
+import kotlin.random.Random
+
+
+class ConfiguracaoDeSorteioFragment : Fragment() {
+
+    private val viewModel: SorteioViewModel by activityViewModels()
+
+    private var _binding: FragmentConfiguracaoDeSorteioBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentConfiguracaoDeSorteioBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            swtNotRepeatNumbers.setOnCheckedChangeListener { _, isChecked ->
+                swtNotRepeatNumbers.trackTintList = getColorStateList(
+                    requireContext(),
+                    if (isChecked) R.color.background_brand else R.color.content_tertiary
+                )
+                viewModel.setShouldRepeatNumbers(shouldRepeatNumbers = !isChecked)
+            }
+
+            etAmountNumbers.addTextChangedListener { text ->
+                viewModel.setDrawAmountNumber(drawAmountNumber = text.toString().toIntOrNull() ?: 0)
+            }
+
+            etInitialLimit.addTextChangedListener { text ->
+                viewModel.setInitialLimit(initialLimit = text.toString().toIntOrNull() ?: 0)
+            }
+
+            etFinalLimit.addTextChangedListener { text ->
+                viewModel.setFinalLimit(finalLimit = text.toString().toIntOrNull() ?: 0)
+            }
+        }
+    }
+
+    fun FragmentResultadoSorteioBinding.gerarTextoDeNumeroSorteado() {
+        val textView = TextView(requireContext()).apply {
+            id = View.generateViewId()
+            text = "${Random.nextInt(100)}"
+            setTextAppearance(R.style.TextAppearance_Sora_DisplayMedium)
+            setTextColor(ContextCompat.getColor(requireContext(), R.color.content_brand))
+        }
+        root.addView(textView)
+        flowResultNumbersHelper.referencedIds = flowResultNumbersHelper.referencedIds.plus(textView.id)
+    }
+
+}
---
+++ app/src/main/java/com/dev/bernardoslailati/sorteadordenumeros/ResultadoSorteioFragment.kt
@@ -0,0 +1,74 @@
+package com.dev.bernardoslailati.sorteadordenumeros
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.dev.bernardoslailati.sorteadordenumeros.databinding.FragmentResultadoSorteioBinding
+import kotlinx.coroutines.launch
+
+
+class ResultadoSorteioFragment : Fragment() {
+
+    private val viewModel: SorteioViewModel by activityViewModels()
+
+    private var _binding: FragmentResultadoSorteioBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentResultadoSorteioBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            lifecycleScope.launch {
+                viewModel.uiState.collect { uiState ->
+                    tvDrawNumber.text =
+                        getString(R.string.numero_do_sorteio, uiState.currentDrawNumber.toString())
+
+                    clearLastDrewNumbers()
+                    uiState.drawNumbers.forEach { drawNumber ->
+                        generateDrawNumberTextView(drawNumber = drawNumber)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun FragmentResultadoSorteioBinding.generateDrawNumberTextView(drawNumber: Int) {
+        val drawNumberTextView = TextView(requireContext()).apply {
+            id = View.generateViewId()
+            text = drawNumber.toString()
+            setTextAppearance(R.style.TextAppearance_RobotoMono_Overline)
+            textSize = 48f
+            setTextColor(ContextCompat.getColor(requireContext(), R.color.content_brand))
+        }
+
+        root.addView(drawNumberTextView)
+        flowResultNumbersHelper.referencedIds =
+            flowResultNumbersHelper.referencedIds.plus(drawNumberTextView.id)
+    }
+
+    private fun FragmentResultadoSorteioBinding.clearLastDrewNumbers() {
+        flowResultNumbersHelper.referencedIds.forEach {
+            root.removeView(root.findViewById(it))
+        }
+    }
+}
